160495005bdf813be2b3278bb5c0ed9278b78283
Richard Thier <u9vata@gmail.com>
2018-11-10 23:55:30 +0100

    Added tweaks to "not think" when tabulating text and to "think" between words

--- asciinemaze.py
+++ asciinemaze.py
@@ -4,6 +4,8 @@ import sys, random, shutil
 
 lospeed = 0.03
 hispeed = 0.4
+thinkscaler = 5
+tabtime = 0.0001
 
 def main(argv):
     if (len(argv) < 1):
@@ -21,18 +23,30 @@ def main(argv):
         # Read all lines of the file
         t=0
         for line in open(argv[0]):
+            prev=' ' # Rem.: For leading tabulation spaces!
             # And add one line for each character
             for c in line:
-                t = t + random.uniform(lospeed, hispeed)
                 sys.stdout.write('[')
+                if ((c == '\t') or (c == ' ')):
+                    # When char is tab or space it might be a source code!
+                    # That means we must skip tabulation spaces and also
+                    # we should wait a bit after end of words (thinking time)
+                    if ((prev == '\t') or (prev == ' ')):
+                        t = t + tabtime # fast tabulation!
+                    else:
+                        t = t + random.uniform(lospeed*thinkscaler, hispeed*thinkscaler) # thinking time
+                else:
+                    t = t + random.uniform(lospeed, hispeed) # usual writin' time
+
                 sys.stdout.write(str(t))
                 sys.stdout.write(', "o", "')
-                if(c == '\n'):
-                    # Used by the file format this way!!!
+                if (c == '\n'):
+                    # Rem.: Endl is used by the file format this way!!!
                     sys.stdout.write('\\r\\n')
                 else:
                     sys.stdout.write(str(c))
                 print('"]')
+                prev = c # update prev char
 
 if __name__ == "__main__":
    main(sys.argv[1:])
