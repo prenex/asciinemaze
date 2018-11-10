b9acda3d006a4e6e531af04beca3a33a9b7406cb
Richard Thier <u9vata@gmail.com>
2018-11-10 23:42:16 +0100

    Using our proper terminal sizes; better speed

--- asciinemaze.py
+++ asciinemaze.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
 
-import sys, random
+import sys, random, shutil
+
+lospeed = 0.03
+hispeed = 0.4
 
 def main(argv):
     if (len(argv) < 1):
         print("Input file is missing!")
     else:
-        # Add header line
-        # TODO: Make width and height parameterizable!
-        print('{"timestamp": 1541886839, "width": 144, "version": 2, "env": {"TERM": "xterm-256color", "SHELL": "/bin/bash"}, "height": 62}')
+        # Create header line
+        # Use the width and height as the terminal is now!
+        col, row = shutil.get_terminal_size()
+        sys.stdout.write('{"timestamp": 1541886839, "width":')
+        sys.stdout.write(str(col))
+        sys.stdout.write(', "version": 2, "env": {"TERM": "xterm-256color", "SHELL": "/bin/bash"}, "height": ')
+        sys.stdout.write(str(row))
+        print('}')
 
         # Read all lines of the file
         t=0
         for line in open(argv[0]):
             # And add one line for each character
             for c in line:
-                t = t + random.uniform(0.1, 1.0)
+                t = t + random.uniform(lospeed, hispeed)
                 sys.stdout.write('[')
                 sys.stdout.write(str(t))
                 sys.stdout.write(', "o", "')
