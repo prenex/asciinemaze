ae617e2d36ef0418118ea061886533300bde73d4
Richard Thier <u9vata@gmail.com>
2018-11-10 23:30:35 +0100

    First implementation

--- asciinemaze.py
+++ asciinemaze.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
 
-import sys, time
+import sys, random
 
 def main(argv):
     if (len(argv) < 1):
         print("Input file is missing!")
     else:
-        # TODO: Add header line
+        # Add header line
+        # TODO: Make width and height parameterizable!
+        print('{"timestamp": 1541886839, "width": 144, "version": 2, "env": {"TERM": "xterm-256color", "SHELL": "/bin/bash"}, "height": 62}')
 
         # Read all lines of the file
+        t=0
         for line in open(argv[0]):
-            # FIXME: And add one line for each character
+            # And add one line for each character
             for c in line:
-                print(c)
-                time.sleep(1)
+                t = t + random.uniform(0.1, 1.0)
+                sys.stdout.write('[')
+                sys.stdout.write(str(t))
+                sys.stdout.write(', "o", "')
+                if(c == '\n'):
+                    # Used by the file format this way!!!
+                    sys.stdout.write('\\r\\n')
+                else:
+                    sys.stdout.write(str(c))
+                print('"]')
 
 if __name__ == "__main__":
    main(sys.argv[1:])
